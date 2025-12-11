29a53438c72e130e08e880e73867565a82f6c870
DESKTOP-T3C92S7\大河原廉 <ren.0503ny@gmail.com>
2025-12-11 23:15:55 +0900

    ファイル構成の改善

--- listing-svc/src/db.js
+++
@@ -1,8 +0,0 @@
-import pg from 'pg';
-const { Pool } = pg;
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
